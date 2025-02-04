5413e1da35a224035b7f26698355335d3c4dc7ef
MrBisp <frederikbisp@gmail.com>
2025-02-04 10:30:52 +0100

    small bug

--- app/api/user/check-credits/route.js
+++ app/api/user/check-credits/route.js
@@ -3,6 +3,8 @@ import { createClient } from '@supabase/supabase-js';
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/libs/next-auth";
 
+export const dynamic = 'force-dynamic';
+
 // Create Supabase client
 const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
